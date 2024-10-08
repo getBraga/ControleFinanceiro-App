23b65b5a6af92d2832f0925803fa2e6c8c117f89
Ysmael Braga <ysmael.braga@ibm.com>
2024-10-08 14:51:23 -0300

    titulo dinamico

--- src/app/shared/titulo/titulo.component.ts
+++ src/app/shared/titulo/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, input, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
@@ -7,6 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   @Input() titulo: string = '';
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subtitulo: string = 'Desde 2024';
+  @Input() mostrarListar: boolean = false;
+  @Input() nomeListar: string = '';
   constructor() {}
 
   ngOnInit() {}
