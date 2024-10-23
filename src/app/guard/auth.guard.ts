26a4665a208132ed2f9f043073dc591be9815c70
Ysmael Braga <ysmael.braga@ibm.com>
2024-10-22 23:12:07 -0300

    adicionado o auth guard

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private toaster: ToastrService) {}
+
+  canActivate(): boolean {
+    if (JSON.parse(localStorage.getItem('user')!).token) return true;
+    console.log('oiiiiiiiiii');
+    this.toaster.info('Usuário não autenticado!');
+    this.router.navigate(['/user/login']);
+    return false;
+  }
+}
