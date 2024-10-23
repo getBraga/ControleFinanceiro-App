e56614920b2dde4352e4e50525fee31e3c5d178e
Ysmael Braga <ysmael.braga@ibm.com>
2024-10-22 23:09:28 -0300

    mudanca e atualizacao do token do usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/account-services/user.service';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ControleFinanceiro-App';
+  constructor(public userService: UserService) {}
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+  setCurrentUser(): void {
+    let user: User = {} as User;
+    if (localStorage.getItem('user')) {
+      user = JSON.parse(localStorage.getItem('user') ?? '{}');
+    }
+
+    this.userService.setCurrentUser(user);
+  }
 }
